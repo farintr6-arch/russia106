ba15ac9fefc86f64d94f00be3a04ca43ba808443
farintr6-arch <farintr6@gmail.com>
2026-02-28 21:17:38 +0700

    Refactor floating symbols code for clarity

--- prikol1.lua
+++ prikol1.lua
@@ -1,17 +1,15 @@
 
--- Летающие символы на экране (GLua)
--- Путь для сохранения: garrysmod/lua/autorun/client/floating_symbol.lua
 
 
 
 
-local symbol = "卐" -- Символ
+local symbol = "卐" 
 local fontName = "FloatingSymbolFontLarge"
-local symbolCount = 20 -- Количество символов на экране
+local symbolCount = 20
 
 
 
--- Создаем крупный шрифт
+
 surface.CreateFont(fontName, {
     font = "Arial",
     size = 250,
@@ -20,7 +18,7 @@ surface.CreateFont(fontName, {
 
 local symbols = {}
 
--- Инициализация символов
+
 local function InitSymbols()
     symbols = {}
     for i = 1, symbolCount do
@@ -44,11 +42,11 @@ hook.Add("HUDPaint", "FloatingSymbolExample", function()
     local tw, th = surface.GetTextSize(symbol)
 
     for _, s in ipairs(symbols) do
-        -- Обновляем позицию
+
         s.x = s.x + s.vx * frameTime
         s.y = s.y + s.vy * frameTime
         
-        -- Отскок от краев
+
         if s.x + tw > sw or s.x < 0 then
             s.vx = -s.vx
             s.x = math.Clamp(s.x, 0, sw - tw)
@@ -59,13 +57,14 @@ hook.Add("HUDPaint", "FloatingSymbolExample", function()
             s.y = math.Clamp(s.y, 0, sh - th)
         end
         
-        -- Отрисовка
+
         draw.SimpleText(symbol, fontName, s.x, s.y, s.col)
     end
 end)
 
--- Переинициализация при смене разрешения
+
 hook.Add("OnScreenSizeChanged", "ResetFloatingSymbols", function()
     InitSymbols()
 end)
+
 
